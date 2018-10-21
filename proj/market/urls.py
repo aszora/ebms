91f99bf5de8f5e23d783a5870b6a72954eb4f258
dsouzajess98 <djessica@student.nitw.ac.in>
2018-10-21 19:15:19 +0530

    Register and Login added

--- proj/market/urls.py
+++ proj/market/urls.py
@@ -3,6 +3,8 @@ from . import views
 
 urlpatterns = [
         url(r'^$', views.stuff, name='stuff'),
+        url(r'^signup/$', views.signup, name='signup'),
+        url(r'^login/$', views.signin, name='login'),
         #url(),  
         
         
--- proj/market/views.py
+++ proj/market/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
 from .models import Post
 from django.utils import timezone
+from django.contrib.auth import authenticate, login, logout
+from django.shortcuts import render, redirect, HttpResponse
+from django.contrib.auth.models import User
 # Create your views here.
 
 def stuff(request):
@@ -8,3 +10,30 @@ def stuff(request):
     posts = Post.objects.all()
     return render(request, 'market/stuff.html', {})
 
+
+def signup(request):
+	response = {}
+	if request.method == 'POST' :
+		username = request.POST['username']
+		password = request.POST['password']
+		if User.objects.filter(username=username):
+			response['error']=1
+		else:
+			User.objects.create_user(username = username,password = password,email='')
+	return render(request,'market/login.html',response)
+
+
+
+def signin(request):
+	response = {}
+	if request.method == 'POST' :
+		username = request.POST['username']
+		password = request.POST['password']
+		user = authenticate(username=username, password=password)
+		if user is None :
+			response['error']=2
+			return render(request,'market/login.html',response)
+		else :
+			login(request,user)
+			return redirect('/market')
+	return render(request,'market/login.html',response)
