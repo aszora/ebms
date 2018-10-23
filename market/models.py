aa5fe6e8f2742bb5b6ec15047b533539b22c8389
aszora <sumanthgarlap@gmail.com>
2018-10-24 01:32:24 +0530

    changed post and added tags

--- market/models.py
+++ market/models.py
@@ -4,11 +4,21 @@ from django.db import models
 from django.utils import timezone
 # Create your models here.
 
+
+class Tags(models.Model):
+    title = models.CharField(max_length=25)
+   
+    def __str__(self):
+	return self.title
+     
 class Post(models.Model):
     pub_date = models.DateField(default=timezone.now)
-    title = models.CharField(max_length=200)
+    title = models.CharField(max_length=200,default=None)
     author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
     desc = models.TextField() #this is the book description
+    doc = models.FileField(upload_to='documents/', default=None,blank=True)
+    #uploaded_at = models.DateTimeField(auto_now_add=True,default=None)
+    tags = models.ManyToManyField(Tags)
 
     def publish(self):
  #       self.pub_date = timezone.now()
